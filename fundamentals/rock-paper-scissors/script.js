17e4c526b029ff1ea1f8524ea62cee5ceabfcfbf
orlambda <115938017+orlambda@users.noreply.github.com>
2024-08-20 18:56:49 +0100

    Add js functions and pseudocode

---
+++ fundamentals/rock-paper-scissors/script.js
@@ -0,0 +1,46 @@
+console.log("Hello World")
+
+// functions
+
+function playGame() {
+
+    for (var i = 0; i < 5; i++)
+        {
+            playRound(getHumanChoice(), getComputerChoice())
+        }        
+
+    // print winner
+    // if draw
+    // else if human wins
+    // else computer wins
+
+    }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    
+    // if same
+        // print
+
+    // else if human wins
+        // print
+
+    // else computer wins
+        // print
+
+}
+
+function getComputerChoice() {
+
+}
+
+function getHumanChoice() {
+
+}
+
+// global variables
+
+var humanScore = 0
+
+var computerScore = 0
+
