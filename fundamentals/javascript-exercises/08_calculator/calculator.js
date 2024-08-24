8fa3d88297f0fac8e18334abe3a20bb2760bfb17
orlambda <115938017+orlambda@users.noreply.github.com>
2024-08-24 16:34:14 +0100

    Implement 08_calculator

--- fundamentals/javascript-exercises/08_calculator/calculator.js
+++ fundamentals/javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+  return sum;	
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  if (a.length === 0) {
+    return "ERROR"
+  }
+  let total = a[0];
+  for (let i = 1; i < a.length; i++) {
+    total *= a[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;	
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) {
+    return 1;
+  }
+  if (n < 0) {
+    return undefined;
+  }
+  if (n === 1) {
+    return n;
+  } else {
+    return (n * (factorial(n-1)))
+  }
 };
 
 // Do not edit below this line
