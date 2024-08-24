8586486d31772d6bbc9b4803c913dc78a2b2b58f
orlambda <115938017+orlambda@users.noreply.github.com>
2024-08-24 15:16:46 +0100

    Delete js-exercises
    Error made with commits so deleting the directory,
    forking again, and re-adding solutions.

--- fundamentals/js-exercises/03_reverseString/reverseString.js
+++
@@ -1,8 +0,0 @@
-const reverseString = function(str) {
-    const arrStr = str.split("");
-    arrStr.reverse();
-    return arrStr.join("");
-};
-
-// Do not edit below this line
-module.exports = reverseString;
