453528a4febc08ef372b33d5d1f114643544a4f7
orlambda <115938017+orlambda@users.noreply.github.com>
2024-08-24 15:39:18 +0100

    Implement 02_repeatString

--- fundamentals/javascript-exercises/02_repeatString/repeatString.js
+++ fundamentals/javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, j) {
+    if (j < 0) {
+        return "ERROR";
+    }
+    output = "";
+    for (var i = 0; i < j; i++) {
+        output += str;
+    }
+    return output;
 };
 
 // Do not edit below this line
