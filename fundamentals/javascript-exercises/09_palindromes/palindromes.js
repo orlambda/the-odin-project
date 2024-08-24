c77b8ddd37884bf0d41f9983b7b2c8bff469cb4e
orlambda <115938017+orlambda@users.noreply.github.com>
2024-08-24 16:52:01 +0100

    Implement 09_palindromes

--- fundamentals/javascript-exercises/09_palindromes/palindromes.js
+++ fundamentals/javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+function isNumAlpha(c) {
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) {
+        return true;
+    }
+    return false;
+}
 
+const palindromes = function (str) {
+    let strA = "";
+    let strB = "";
+    for (let c of str) {
+        if (isNumAlpha(c)) {
+            strA = strA + c.toLowerCase();
+            strB = c.toLowerCase() + strB;
+        }
+    }
+    if (strA === strB) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
