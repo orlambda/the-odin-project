7db6405ebef0eef1d944a63df6e7f59754c45af0
orlambda <115938017+orlambda@users.noreply.github.com>
2024-08-24 16:01:03 +0100

    Implement 07_tempConversion

--- fundamentals/javascript-exercises/07_tempConversion/tempConversion.js
+++ fundamentals/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,7 +1,34 @@
-const convertToCelsius = function() {
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+const convertToCelsius = function(f) {
+  // Check for finite number
+  if (!Number.isFinite(f)) {
+    return "ERROR"
+  }
+  // Check is above absolute zero
+  if (f < -459.67) {
+    return "ERROR"
+  }
+  var c = (f - 32) * 5/9
+  return round(c, 1);
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  // Check for finite number
+  if (!Number.isFinite(c)) {
+    return "ERROR"
+  }
+  // Check is above absolute zero
+  if (c < -273.15) {
+    return "ERROR"
+  }
+  var f = (c * 9/5) + 32
+  return round(f, 1);
 };
 
 // Do not edit below this line
