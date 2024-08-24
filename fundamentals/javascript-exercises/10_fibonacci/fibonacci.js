b243bc8dfd63dd7e240b5e17437416afe3d63046
orlambda <115938017+orlambda@users.noreply.github.com>
2024-08-24 17:07:38 +0100

    Implement 10_fibonacci

--- fundamentals/javascript-exercises/10_fibonacci/fibonacci.js
+++ fundamentals/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n === 0) {
+        return 0;
+    }
+    if (n < 0) {
+        return "OOPS";
+    }
+    if (n === 1 || n === 2) {
+        return 1;
+    }
+    let a = 1;
+    let b = 1;
+    let c = a + b;
+    for (let i = 3; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
 };
 
 // Do not edit below this line
