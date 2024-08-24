e9144897711ac965b8710a19b426557a92f122be
orlambda <115938017+orlambda@users.noreply.github.com>
2024-08-24 14:50:26 +0100

    Complete 04_removeFromArray

--- fundamentals/js_arrays/04_removeFromArray/removeFromArray.js
+++ fundamentals/js_arrays/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...vals) {
+    for (let v of vals) {
+        while (true) {
+            i = arr.indexOf(v);
+            if (i >= 0) {
+                arr.splice(i, 1);
+            } else {
+                break;
+            }
+        }
+    }
+    return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
