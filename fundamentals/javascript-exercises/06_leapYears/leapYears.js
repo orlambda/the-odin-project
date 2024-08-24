60385d8500a49c54b5cfc65e5bb923e184f9609a
orlambda <115938017+orlambda@users.noreply.github.com>
2024-08-24 15:43:43 +0100

    Implement 06_leapYears

--- fundamentals/javascript-exercises/06_leapYears/leapYears.js
+++ fundamentals/javascript-exercises/06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+/*
+Leap years are years divisible by four (like 1984 and 2004).
+However, years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible by 400,
+    (like 1600 and 2000, which were in fact leap years).
+    */
+    // Must be a positive integer
+    if (!Number.isInteger(year) || year < 0) {
+        return false;
+    }
+    if (!(year % 4) &&
+        (((year % 100) || !(year % 400) ))) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
