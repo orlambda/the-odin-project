fee8d7fb08070088a28cf303c96b063f46e4b3dc
orlambda <115938017+orlambda@users.noreply.github.com>
2024-08-26 01:03:56 +0100

    Implement 12_findTheOldest

--- fundamentals/javascript-exercises/12_findTheOldest/findTheOldest.js
+++ fundamentals/javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,42 @@
-const findTheOldest = function() {
+function calculateAge (person) {
+    const currentYear = new Date().getUTCFullYear();
+    if ('yearOfDeath' in person) {
+        return person.yearOfDeath - person.yearOfBirth;
+    } else {
+        return currentYear - person.yearOfBirth;
+    }
+}
 
+const findTheOldest = function(people) {
+    let highestAge = 0;
+    let oldestPerson = undefined;
+    for (let person of people) {
+        let currentAge = calculateAge(person);
+        if (currentAge > highestAge) {
+            highestAge = currentAge;
+            oldestPerson = person;
+        }
+    }
+    return oldestPerson;
 };
 
+
+console.log(findTheOldest([
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]).name)
 // Do not edit below this line
 module.exports = findTheOldest;
