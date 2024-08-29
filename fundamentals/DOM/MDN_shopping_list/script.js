8be50d465c82b5dfffba78e2cbe4dbcc9df12a41
orlambda <115938017+orlambda@users.noreply.github.com>
2024-08-29 20:30:24 +0100

    Implement "enter new item" form
    Disallows items with no name
    Allows whitespace-only names

--- fundamentals/DOM/MDN_shopping_list/script.js
+++ fundamentals/DOM/MDN_shopping_list/script.js
@@ -1,1 +1,28 @@
-console.log("heya")
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  
+  const myItem = input.value;
+  if (myItem === "") {
+    return
+  }
+  input.value = '';
+
+  const listItem = document.createElement('li');
+  const listText = document.createElement('span');
+  const listBtn = document.createElement('button');
+
+  listItem.appendChild(listText);
+  listText.textContent = myItem;
+  listItem.appendChild(listBtn);
+  listBtn.textContent = 'Delete';
+  list.appendChild(listItem);
+
+  listBtn.addEventListener('click', () => {
+    list.removeChild(listItem);
+  });
+
+  input.focus();
+})
